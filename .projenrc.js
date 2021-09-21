81c4399679f9feabe1228dd50ab2561228a33248
securitywing <syras@securitywing.com>
2021-09-21 19:26:36 +0000

    test12

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,18 @@ const project = new JsiiProject({
   author: 'securitywing',
   authorAddress: 'syras@securitywing.com',
   defaultReleaseBranch: 'main',
-  name: 's3newmodule',
-  repositoryUrl: 'https://github.com/syras/s3newmodule.git',
+  name: '@cdksecuritywing/s3newmodule',
+  repositoryUrl: 'https://github.com/cdksecuritywing/s3newmodule.git',
+  jest: false,
+  devDeps: [
+    'fs-extra',
+    '@types/fs-extra@^8',
+
+  ],
+  deps: ['projen'],
+  peerDeps: ['projen'],
+  npmDistTag: 'latest',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
